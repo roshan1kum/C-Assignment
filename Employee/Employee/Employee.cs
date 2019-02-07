7a8fd5eef8f30fa87edcda59735bb9392c79e0ed
Roshan K <roshankumar@promactinfo.com>
2019-02-07 11:09:05 +0530

    C#assignment

---
+++ Employee/Employee/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        public event methodDelegate methodCalled;
+        private int ID
+        {
+            get; set;
+        }
+        private string Name
+        {
+            get; set;
+        }
+        private string DepartmentName
+        {
+            get; set;
+        }
+        public Employee(int ID, string Name, string DepartmentName)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.DepartmentName = DepartmentName;
+        }
+        public int GetId()
+        {
+            methodCalled("GetID");
+            return ID;
+
+        }
+        public string GetName()
+        {
+            methodCalled("GetName");
+            return Name;
+        }
+        public string GetDepartmentName()
+        {
+            methodCalled("GetDepartmentName");
+            return DepartmentName;
+        }
+        public int GetId(int id)
+        {
+            ID = id;
+            return ID;
+        }
+        public string GetName(string name)
+        {
+            Name = name;
+            return Name;
+        }
+        public string GetDepartmentName(string dept)
+        {
+            DepartmentName = dept;
+            return  DepartmentName;
+        }
+        
+    }
+}
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name, departmentName;
+            Console.WriteLine("enter the id:");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the Name");
+            name = Console.ReadLine();
+            Console.WriteLine("enter the DepartmentName");
+            departmentName = Console.ReadLine();
+
+            Employee emp = new Employee(id, name, departmentName);
+            emp.methodCalled += new methodDelegate(MethodCalling);
+            Console.WriteLine("ID:" + emp.GetId());
+            Console.WriteLine("Name:" + emp.GetName());
+            Console.WriteLine("Department name:" + emp.GetDepartmentName());
+
+            Console.WriteLine("enter the updated id:");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the updated Name");
+            name = Console.ReadLine();
+            Console.WriteLine("enter the updated DepartmentName");
+            departmentName = Console.ReadLine();
+            
+
+            Console.WriteLine("updated ID:" + emp.GetId(id));
+            Console.WriteLine("updated Name:" + emp.GetName(name));
+            Console.WriteLine("updated Department name:" + emp.GetDepartmentName(departmentName));
+
+        }
+        static void MethodCalling(string mName)
+         {
+             Console.WriteLine(mName + " Method Called");
+         }
+
+    }
+}
