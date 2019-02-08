c17ba4d5b70813a46f3d6d0e71158ec151944e3c
Roshan K <roshankumar@promactinfo.com>
2019-02-08 15:00:43 +0530

    assignment 4

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] arr = { "Boat", "house", "cat", "river", "cupboard" }
+            var array = new List<string>() { "Boat", "house", "cat", "river", "cupboard" };
+            query q = new query(array);
+            Console.WriteLine("the plural of all words are:");
+            q.plural();
+            array[1] = "apartment";
+            Console.WriteLine("\n");
+            Console.WriteLine("the list are:");
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+            array.Add("lengths");
+            Console.WriteLine("the length of the list is:" + array.Count);
+            Console.WriteLine("\n");
+            Console.WriteLine("enter the  length of characters");
+            int n = int.Parse(Console.ReadLine());
+            var result = array.Where(s => s.Length == n);
+            foreach(var i in result)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("the word on 3rd position are:");
+            Console.WriteLine(array[2]);
+            Console.WriteLine("\n");
+            Console.WriteLine("the word in ascending are:");
+            var asce = from s in array
+                         orderby s ascending
+                         select s;
+            foreach(var i in asce)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("the reverse word are :");
+            var rev = array.OrderByDescending(a => a);
+            foreach(var i in rev)
+            {
+                Console.WriteLine(i);
+           }
+        }
+    }
+}
---
+++ Dictionary/Dictionary/query.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class query
+    {
+        List<string> arr = new List<string>();
+        public query(List<string> arr)
+        {
+            this.arr = arr;
+        }
+        public void plural()
+        {
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(arr[i] + "s");
+            }
+        }
+        //public IEnumerable<string> lengthofCharacter(int n)
+        //{
+        //    var len = from v in array
+        //              where v.Length = n
+        //              select v;
+        //}
+        public void printList()
+        {
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
